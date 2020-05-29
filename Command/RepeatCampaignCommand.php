cb793b6ed556a53d4c43c9077c2c4a414db62a22
Agnete Moos <agms@sonderborg.dk>
2020-05-29 12:55:06 +0200

    Working solution

--- Command/RepeatCampaignCommand.php
+++ Command/RepeatCampaignCommand.php
@@ -14,7 +14,6 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Output\OutputInterface;
 use Os2Display\CampaignBundle\Entity\Campaign;
-use \Datetime;
 
 /**
  * Class RepeatCampaignCommand
@@ -30,7 +29,7 @@ class RepeatCampaignCommand extends ContainerAwareCommand
   {
     $this
       ->setName('bibsdb:campaign:schedule')
-      ->setDescription('Schedule campaign to run every day. Provide startdate and time plus duration. Distinguish between weekdays and weekends.)
+      ->setDescription('Schedule campaign to run every day. Provide startdate and time plus duration. Distinguish between weekdays and weekends.')
       ->addArgument(
         'title',
         InputArgument::REQUIRED,
@@ -57,9 +56,9 @@ class RepeatCampaignCommand extends ContainerAwareCommand
           'When to stop the repeating campaign? Format is d-m-Y G:i. This is an example: 21-02-2020 21:45.'
       )
       ->addArgument(
-          'channal',
+          'channel',
           InputArgument::REQUIRED,
-          'Enter the id of the channal to be run.'
+          'Enter the id of the channl to be run.'
       )
       ->addArgument(
           'screengroup',
@@ -71,30 +70,33 @@ class RepeatCampaignCommand extends ContainerAwareCommand
 
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    $date = DateTime::createFromFormat('d-m-Y G:i', $input->getArgument('startdatetime'));
-    $enddate = DateTime::createFromFormat('d-m-Y G:i', $input->getArgument('enddatetime'));
-    $duration = $input->getArgument('enddatetime');
+    $doctrine = $this->getContainer()->get('doctrine');
+    $em = $doctrine->getEntityManager('default');
 
+    $date = \DateTime::createFromFormat('d-m-Y G:i', $input->getArgument('startdatetime'));
+    $enddate = \DateTime::createFromFormat('d-m-Y G:i', $input->getArgument('enddatetime'));
+    $duration = $input->getArgument('duration');
 
     while ($date < $enddate) {
+      
       // Is $date weekday or weekend?
       $weekday_or_weekend = ($date->format('N') >= 6 ? "weekend" : "weekday"); 
     
       if ($weekday_or_weekend == $input->getArgument('weekday_or_weekend')) {
         // Create campaign
-        $doctrine = $this->getContainer()->get('doctrine');
-        $em = $doctrine->getEntityManager('default');
         $campaign = new Campaign();
-
-        // Title
-        $campaign->setTitle($input->getArgument('title'));
+        $campaign->setCreatedAt(new \DateTime());
+        $campaign->setUpdatedAt(new \DateTime());
 
         // SceduleFrom
-        $campaign->setSceduleFrom($date);
+        $campaign->setScheduleFrom($date);
 
         // ScheduleTo
-        $sceduleTo = (clone $date).add(new DateInterval('PT' . $duration . 'M'));
-        $campaign->setSceduleTo($sceduleTo);
+        $sceduleTo = (clone $date)->add(new \DateInterval('PT' . $duration . 'M'));
+        $campaign->setScheduleTo($sceduleTo);
+
+        // Title
+        $campaign->setTitle($input->getArgument('title') . " " . $date->format('d-m-Y G:i'));
 
         // User
         $campaign->setUser(NULL);
@@ -102,18 +104,20 @@ class RepeatCampaignCommand extends ContainerAwareCommand
         // Description
         $campaign->setDescription("Repeated campaign");
 
-        // Channals
-        $campaign->setChannals = new ArrayCollection([$input->getArgument('channal')]);
-
-        // Screens
-        $campaign->setScreens = new ArrayCollection([]);
-
-        // ScreenGroups
-        $campaign->setScreenGroups = new ArrayCollection([$input->getArgument('screengroup')]);
-
         // Save campaign
-        $em->persist($campaign);
+        $thing = $em->persist($campaign);
         $em->flush();
+
+        // Relate campaign with screen group - ugly sql
+        $sql = "INSERT INTO ik_campaign_group (campaign_id, group_id) VALUES (" . $campaign->getId() . ", " . $input->getArgument('screengroup') . ")";
+        $stmt = $em->getConnection()->prepare($sql);
+        $result = $stmt->execute();
+
+        // Relate campaign with channal - ugly sql
+        $sql = "INSERT INTO ik_campaign_channel (campaign_id, channel_id) VALUES (" . $campaign->getId() . ", " . $input->getArgument('channel') . ")";
+        $stmt = $em->getConnection()->prepare($sql);
+        $result = $stmt->execute();
+
         $output->writeln('Created campaign from ' . $date->format('d-m-Y G:i') . ' to ' . $sceduleTo->format('d-m-Y G:i'));
       }
 
